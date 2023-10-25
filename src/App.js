1fcae458af6ff093b61f4408532ae0119144c3e5
Sudeep-K <pong32btl@gmail.com>
2023-10-25 20:50:39 +0545

    components and pages initialization

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import { CssBaseline } from '@mui/material';
+
+import { Actors, MovieInformation, Movies, Profile } from './pages';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div>
       <CssBaseline />
-      <h1>Hello, world!</h1>
+      <NavBar />
+      <main>
+        <Routes>
+          <Route path='/' element={<Movies />} />
+          <Route exact path='/movie/:id' element={<MovieInformation />} />
+          <Route exact path='/actors/:id' element={<Actors />} />
+          <Route exact path='/profile/:id' element={<Profile />} />
+        </Routes>
+      </main>
     </div>
   );
 }
